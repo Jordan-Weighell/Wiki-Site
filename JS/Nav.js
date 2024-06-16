259a3b9f44c1b6b38096d24e46a1efc172551010
Jordan-Weighell <Dyscolour@gmail.com>
2024-06-16 15:00:35 +0100

    Templates-Start

---
+++ JS/Nav.js
@@ -0,0 +1,8 @@
+var document;
+<script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+<script>
+     $(function(){ 
+         $('#header').load('Header.html');
+         $('#footer').load('footer.html');
+     });
+</script>
---
+++ JS/header.js
@@ -0,0 +1,10 @@
+<body>
+
+$(function(){
+    $("[data-load]").each(function(){
+        $(this).load($(this).data("load"), function(){
+        });
+    });
+})
+
+</body>
